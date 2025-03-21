c2a79e3b1e9ee61c90ce761267a1e9fac51568f6
DaviZCodes <davi.bestguy@gmail.com>
2025-03-21 16:55:58 -0400

    created a flask backend api

--- main.py
+++ main.py
@@ -1,33 +1,59 @@
+import time 
 import json 
+from flask import Flask, jsonify
 
 from mal_api.mal_user_animelist_api import * 
 from mal_api.mal_user_mangalist import *  
 
 from web_scraper_beautifulsoup.web_scraper_profile import getUserProfile 
 
-USER = "Xinil"
-URL = f"https://myanimelist.net/profile/{USER}"
+from openai_api.openai_api import generateRoast
 
-user_profile = getUserProfile(URL) 
-user_animelist = getAnimeList(USER)
-user_top20_animelist = getUserTop20AnimeList(USER)
-user_bottom20_animelist = getUserBottom20AnimeList(USER) 
-user_top20_mangalist = getUserTop20MangaList(USER) 
-user_bottom20_mangalist = getUserBottom20MangaList(USER) 
+app = Flask(__name__)
 
-# json data 
-user_output = {
-    "user" : USER,
-    "user_url" : URL, 
-    "user_profile": user_profile,
-    "user_top20_animelist": user_top20_animelist,
-    "user_bottom20_animelist": user_bottom20_animelist, 
-    "user_top20_mangalist": user_top20_mangalist,
-    "user_bottom20_mangalist": user_bottom20_mangalist
-}
+def fetchMalProfile(username): 
+    url = f"https://myanimelist.net/profile/{username}"
 
-# put all of the information to a .txt file 
-output_file = "user_output.txt" 
+    user_profile = getUserProfile(url) 
+    user_animelist = getAnimeList(username)
+    user_top20_animelist = getUserTop20AnimeList(username)
+    user_bottom20_animelist = getUserBottom20AnimeList(username) 
+    user_top20_mangalist = getUserTop20MangaList(username) 
+    user_bottom20_mangalist = getUserBottom20MangaList(username) 
 
-with open(output_file, "w") as file: 
-    json.dump(user_output, file, indent=3)
+    # json data 
+    user_output = {
+        "user" : username,
+        "user_url" : url, 
+        "user_profile": user_profile,
+        "user_top20_animelist": user_top20_animelist,
+        "user_bottom20_animelist": user_bottom20_animelist, 
+        "user_top20_mangalist": user_top20_mangalist,
+        "user_bottom20_mangalist": user_bottom20_mangalist
+    }
+
+    # put all of the information to a .txt file 
+    output_file = "user_output.txt" 
+
+    with open(output_file, "w") as file: 
+        json.dump(user_output, file, indent=3)
+    
+    return user_output
+
+def getRoast():
+    mal_profile = fetchMalProfile("Xinil")
+
+    time.sleep(3)
+
+    roast = generateRoast()
+
+    return roast
+
+@app.route("/roast", methods=["GET"])
+def roastProfile(): 
+    roast = getRoast() 
+
+    return jsonify(roast) 
+
+if __name__ == "__main__": 
+    app.run(debug=True)
--- openai_api/openai_api.py
+++ openai_api/openai_api.py
@@ -12,27 +12,28 @@ client = OpenAI(
     api_key=GITHUB_TOKEN,
 )
 
-file = open("user_output.txt", "r")
+def generateRoast(): 
+    file = open("user_output.txt", "r")
 
-file_content = file.read()
+    file_content = file.read()
 
-prompt = "Can you roast this user's MAL profile? Make sure to laugh at the user and make them feel like a loser. You don't have to mention every piece of information I included, but just roast some useful and relevant things. Make sure the roast is not that long. Make fun of the user for their taste, call the user arrogant for giving low scores, and tell the user to go touch grass if they watch too much anime. Do not include anything offensive like slurs though." + file_content
+    prompt = "Can you roast this user's MAL profile? Make sure to laugh at the user and be funny and sarcastic. You don't have to mention every piece of information I included, but just roast some useful and relevant things. Make sure the roast is not that long. Make fun of the user for their taste, call the user arrogant for giving low scores, and tell the user to go touch grass if they watch too much anime. Do not include anything offensive like slurs though." + file_content
 
-response = client.chat.completions.create(
-    messages=[
-        {
-            "role": "system",
-            "content": "",
-        },
-        {
-            "role": "user",
-            "content": prompt,
-        }
-    ],
-    model="gpt-4o",
-    temperature=1,
-    max_tokens=4096,
-    top_p=1
-)
+    response = client.chat.completions.create(
+        messages=[
+            {
+                "role": "system",
+                "content": "",
+            },
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        ],
+        model="gpt-4o",
+        temperature=1,
+        max_tokens=4096,
+        top_p=1
+    )
 
-print(response.choices[0].message.content)
+    return response.choices[0].message.content
